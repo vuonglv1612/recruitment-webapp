53463c1eda70acbcc09c5ecba7cd30323ea85622
vuonglv <it.vuonglv@gmail.com>
2020-10-20 17:09:30 +0700

    🆕 persist redux

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker';
-import store from 'src/redux/store';
+import { store, persistor } from 'src/redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App';
 
 ReactDOM.render((
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 ), document.getElementById('root'));
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,7 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from 'src/redux/reducers';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+    key: 'root',
+    storage,
+}
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store)
